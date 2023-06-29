e26109c6d0675717baaa6105fadbccf50486440b
vijju0007 <138023610+vijju0007@users.noreply.github.com>
2023-06-29 10:39:17 +0530

    Add files via upload

---
+++ PoslogAggregrator/src/com/example/microservices/serviceone/ServiceOneApplication.java
@@ -0,0 +1,14 @@
+package com.example.microservices.serviceone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ServiceOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceOneApplication.class, args);
+    }
+}
